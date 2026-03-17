7ee3fed5d56aa51ae7293c011db17fe4352f09e3
Anastasia <anastasiaerofeeva121@gmail.com>
2026-03-17 18:54:17 +0300

    upd shadcn ui components

--- src/shared/components/ui/Button.tsx
+++ src/shared/components/ui/Button.tsx
@@ -1,9 +1,8 @@
 /* eslint-disable react-refresh/only-export-components */
-"use client"
-
-import { cn } from "@/shared/lib"
-import { Button as ButtonPrimitive } from "@base-ui/react/button"
+import * as React from "react"
 import { cva, type VariantProps } from "class-variance-authority"
+import { Slot } from "radix-ui"
+import { cn } from "@/shared/lib/clsx"
 
 
 const buttonVariants = cva(
@@ -40,11 +39,19 @@ function Button({
   className,
   variant = "default",
   size = "default",
+  asChild = false,
   ...props
-}: ButtonPrimitive.Props & VariantProps<typeof buttonVariants>) {
+}: React.ComponentProps<"button"> &
+  VariantProps<typeof buttonVariants> & {
+    asChild?: boolean
+  }) {
+  const Comp = asChild ? Slot.Root : "button"
+
   return (
-    <ButtonPrimitive 
+    <Comp
       data-slot="button"
+      data-variant={variant}
+      data-size={size}
       className={cn(buttonVariants({ variant, size, className }))}
       {...props}
     />
