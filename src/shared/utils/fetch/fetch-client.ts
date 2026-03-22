de82aa2f17aa76747d195836f31ad986fea86c38
Anastasia <anastasiaerofeeva121@gmail.com>
2026-03-22 23:50:55 +0300

    add methods

--- src/shared/utils/fetch/fetch-client.ts
+++ src/shared/utils/fetch/fetch-client.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { FetchError } from "./fetch-error"
 import { RequestOptions, TypeSearchParams } from "./fetch-types"
 
 export class FetchClient {
@@ -52,7 +54,69 @@ export class FetchClient {
 
     const config:RequestInit = {
       ...options,
-      ...
+      ...(!!this.options && {...this.options}),
+      method,
+      headers: {
+        ...(!!options?.headers && options.headers),
+        ...this.headers
+      }
     }
+
+    const response:Response = await fetch(url, config);
+
+    if(!response.ok) {
+      const error = (await response.json()) as
+      | { message: string}
+      | undefined
+
+      throw new FetchError(response.status, error?.message || response.statusText)
+    }
+
+    if(response.headers.get('Content-Type')?.includes('application/json')) {
+      return (await response.json()) as unknown as T
+    } else {
+      return (await response.text()) as unknown as T
+    }
+  }
+
+  public get<T>(endpoint: string, options: Omit<RequestOptions, 'body'> = {}) {
+    return this.request<T>(endpoint, "GET", options)
+  }
+
+  public post<T>(endpoint: string, body?:Record<string, string>, options: RequestOptions = {}) {
+    return this.request<T>(endpoint, 'POST', {
+      ...options,
+      headers: {
+        'Content-Type': 'application/json',
+        ...(options?.headers || {})
+      },
+      ...(!!body && {body: JSON.stringify(body)})
+    })
+  }
+
+  public put<T>(endpoint: string, body?:Record<string, any>, options: RequestOptions = {}) {
+    return this.request<T>(endpoint, 'PUT', {
+      ...options,
+      headers: {
+        'Content-Type': 'application/json',
+        ...(options?.headers || {})
+      },
+      ...(!!body && {body: JSON.stringify(body)})
+    })
+  }
+
+  public delete<T>(endpoint: string, options: Omit<RequestOptions, 'body'> = {}) {
+    return this.request<T>(endpoint, "DELETE", options)
+  }
+
+  public patch<T>(endpoint: string, body?:Record<string, any>, options: RequestOptions = {}) {
+    return this.request<T>(endpoint, 'PATCH', {
+      ...options,
+      headers: {
+        'Content-Type': 'application/json',
+        ...(options?.headers || {})
+      },
+      ...(!!body && {body: JSON.stringify(body)})
+    })
   }
 }
